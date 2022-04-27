902ba2511d4185eaca2a2b6e7a890f4a6f60ce1f
Ritesh1511 <riteshpc12@gmail.com>
2022-04-27 19:09:48 +0530

    nice

---
+++ realApp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text,View,SafeAreaView } from 'react-native';
+
+const App=()=>{
+  return(
+      <SafeAreaView>
+     <View>
+         <Text>
+             Home
+         </Text>
+     </View>
+     </SafeAreaView>
+  ) 
+};
+export default App;
---
+++ screens/index.js
@@ -0,0 +1,7 @@
+import Home from './Home.js';
+import Restaurant from './Restaurant.js';
+import OrderDelivery from './OrderDelivery.js';
+
+export {
+    Home,Restaurant,OrderDelivery
+}
